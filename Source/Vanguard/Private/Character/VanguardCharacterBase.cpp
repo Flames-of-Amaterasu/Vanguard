5c236cd941a81c00f7baea7e05c007548c8dd369
Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>
2025-09-30 22:36:16 +0100

    Character & Goblin Int BP (7)
    For some reason her SKM refuses to have a socket but Spear can have one so will need to monitor for if this matter later.

--- Source/Vanguard/Private/Character/VanguardCharacterBase.cpp
+++ Source/Vanguard/Private/Character/VanguardCharacterBase.cpp
@@ -6,6 +6,9 @@
 AVanguardCharacterBase::AVanguardCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AVanguardCharacterBase::BeginPlay()
